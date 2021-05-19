5886c8efd9761fc140c9341172d23a5d9e3a89a5
gaomingyan <2310212909@qq.com>
2021-05-19 09:38:25 +0800

    "周阳面试题第一季 新建分支"

--- src/main/java/com/gao/spring/MyTest.java
+++ src/main/java/com/gao/spring/MyTest.java
@@ -1,12 +1,15 @@
 package com.gao.spring;
 
-import com.gao.bean.Person;
+import com.gao.annotationAop.Person;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.FileNotFoundException;
+
 public class MyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Person person = context.getBean("person", Person.class);
         System.out.println(person.getName());
+
     }
 }
--- src/main/java/com/gao/threadpool/MyThreadPoolDemo2.java
+++ src/main/java/com/gao/threadpool/MyThreadPoolDemo2.java
@@ -3,17 +3,22 @@ package com.gao.threadpool;
 import java.util.concurrent.*;
 
 /**
- * æ‰‹å†™çº¿ç¨‹æ± 
+ * ÊÖÐ´Ïß³Ì³Ø
  */
 public class MyThreadPoolDemo2 {
     public static void main(String[] args) {
-        ExecutorService threadPool  = new ThreadPoolExecutor(2, 5, 1L, TimeUnit.SECONDS
-                , new LinkedBlockingQueue<Runnable>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
+        ExecutorService threadPool = new ThreadPoolExecutor(1, 1, 1L, TimeUnit.SECONDS
+                , new LinkedBlockingQueue<Runnable>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
         try {
             for (int i = 0; i < 10; i++) {
-                //threadPool.execute(()->{
-                //    System.out.println(Thread.currentThread().getName()+"\t åŠžç†");
-                //});
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t °ìÀí");
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
             Thread.sleep(1000);
         } catch (Exception e) {
